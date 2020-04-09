4652e681472459f0d304aea39a672711f13b0cc4
acdalal <adalal@carleton.edu>
2020-04-09 07:33:28 -0500

    added query with comments; updated script to process files

--- KaggleData/processKaggleData.py
+++ KaggleData/processKaggleData.py
@@ -56,15 +56,18 @@ def removeEndlines(str):
     Removes extraneous \n's from a string, and returns the modified string
     '''
     cleanedStr = str.replace('\n', ' ')
+    # Note: sometimes we'll see escaped string in the text; get rid of those, too.
+    cleanedStr = cleanedStr.replace('\\n', ' ')
     return cleanedStr
 
 
 def main():
-    line = '<p>I am using Red Hat 8 (rhel8), my home router is Asus AC5300 running OpenVPN server. But my rhel8 VPN in Network Manager can\'t not connect to my OpenVPN Server.</p>\n\n<p>Here is the error message I got:  </p>\n\n<blockquote>\n  <p>[root@my-machine ~]# journalctl -f\n  nm-openvpn[30404]: TLS error: Unsupported protocol. This typically indicates that client and server have no common TLS version enabled. This can be caused by mismatched tls-version-min and tls-version-max options on client and server. If your OpenVPN client is between v2.3.6 and v2.3.2 try adding tls-version-min 1.0 to the client configuration to use TLS 1.0+ instead of TLS 1.0 only</p>\n  \n  <p>[root@my-machine ~]# openvpn --version<br>\n  OpenVPN 2.4.7 x86_64-redhat-linux-gnu</p>\n</blockquote>\n\n<p>I\'ve tried by adding <code>tls-version-min 1.0</code> to my <code>.ovpn</code> file but still not working.</p>\n\n<p>Note: In Linux Ubuntu it is working just fine, BUT not Red Hat 8</p>'
-    cleanedUpLine = removeEndlines(line)
-    cleanedUpLine = removeCodeTags(cleanedUpLine)
-    cleanedUpLine = removeHTMLTags(cleanedUpLine)
-    print(cleanedUpLine)
+    with open("queryQandAandComments.txt", "r") as file:
+        lines = file.readlines()
+        cleanedLines = cleanData(lines)
+        for line in cleanedLines:
+            print(line)
+    
 
 if __name__ == '__main__':
     main()
