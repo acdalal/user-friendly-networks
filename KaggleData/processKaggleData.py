b597aeef799f3393ff170e5d15d361d8d26d8a96
acdalal <adalal@carleton.edu>
2020-04-10 07:26:45 -0500

    Updated processing script: write cleaned data to file, place q and a and comments into separate dictionaries.

--- KaggleData/processKaggleData.py
+++ KaggleData/processKaggleData.py
@@ -9,9 +9,50 @@ author: Amy Csizmar Dalal
 creation date: 6 April 2020
 '''
 import re
+import sys
 # TODO: is there an HTML processing module so that we don't have to use regex to remove tags?
 
-# TODO: read in lines from file and process all lines at once
+def storeAllData(lines):
+    '''
+    Separates out the data into questions, answers, and comments, and stores each of them   
+    separately, with the question post ID as the storage key.
+
+    PARAMETER:
+        a list of lines read in from the source file, cleaned up
+    
+    RETURNS:
+        three dictionaries, one each to store questions, answers, and comments 
+    '''
+    questions = {}
+    answers = {}
+    comments = {}
+    currPostID = 0
+    lineItemCount = 0
+
+    for item in lines:
+        if item[0] == '[':
+            # this is a post ID
+            currPostID = int(item[1:])
+            lineItemCount = 1
+        else:
+            if lineItemCount == 1:
+                addToQuestions(currPostID, item, questions)
+            lineItemCount += 1
+
+    return questions, answers, comments
+
+def addToQuestions(postID, item, questions):
+    '''
+    adds the question, unless it's a duplicate, to the questions dictionary
+
+    PARAMETERS:
+        postID, the ID number associated with the post
+        item, the question associated with the post ID
+        questions, the dictionary storing the questions. This will be updated if the question is not already stored in the dictionary.
+    '''
+    if postID not in questions:
+        questions[postID] = item
+    
 
 def cleanData(lines):
     '''
@@ -62,12 +103,21 @@ def removeEndlines(str):
 
 
 def main():
-    with open("queryQandAandComments.txt", "r") as file:
+    if len(sys.argv) < 2:
+        sys.exit("usage: python3 processKaggleData.py filename")
+    with open(sys.argv[1], "r") as file:
         lines = file.readlines()
         cleanedLines = cleanData(lines)
+        
+    '''outfileName = "cleaned/" + sys.argv[1]
+    with open(outfileName, "w") as outfile:
         for line in cleanedLines:
-            print(line)
-    
+            outfile.write(line)'''
+
+    questions, answers, comments = storeAllData(cleanedLines)
+    print(questions)
+    print(answers)
+    print(comments)
 
 if __name__ == '__main__':
     main()
